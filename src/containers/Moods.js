06be74008a3bb50ace4cbb2f32c50b9f323e5d2f
Matt Munch <mattmunch94@gmail.com>
2020-02-16 15:38:36 -0800

    refactor mood js and hook up store

--- src/containers/Moods.js
+++ src/containers/Moods.js
@@ -1,38 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Controls from '../components/controls/Controls';
 import Face from '../components/face/Face';
+import { takeNap, drinkCoffee, study, eatSnack } from '../actions/moodActions';
+import { getMood, getFace } from '../selectors/moodSelectors';
 
-export const isTired = state => state.coffees < 1 && state.naps < 1;
-export const isHyper = state => state.coffees > 3;
-export const isEducated = state => state.studies > 2;
-export const isHungry = state => state.snacks < 1;
+const Moods = () => {
 
-export const getFace = state => {
-  if(isTired(state) && isHungry(state)) return '🤬';
-  if(isHyper(state) && isHungry(state)) return '🤮';
-  if(isTired(state)) return '😴';
-  if(isHyper(state)) return '🙀';
-  if(isEducated(state)) return '🤯';
-  if(isHungry(state)) return '😡';
+  const dispatch = useDispatch();
+  const { coffees, snacks, naps, studies } = useSelector(getMood);
+  const face = useSelector(getFace);
 
-  return '😀';
+  return (
+    <>
+      <Controls>
+        <button onClick={() => dispatch(drinkCoffee())}>Coffee - {coffees}</button>
+        <button onClick={() => dispatch(eatSnack())}>Snacks - {snacks}</button>
+        <button onClick={() => dispatch(takeNap())}>Naps - {naps}</button>
+        <button onClick={() => dispatch(study())}>Studies - {studies}</button>
+      </Controls>
+      <Face emoji={face} />
+    </>
+  );
 };
-
-export default class Moods extends Component {
-  render() {
-    const { coffees, snacks, naps, studies } = this.state;
-    const face = getFace(this.state);
-
-    return (
-      <>
-        <Controls>
-          <button onClick={() => this.handleSelection({ type: 'DRINK_COFFEE' })}>coffee - {coffees}</button>
-          <button onClick={() => this.handleSelection({ type: 'EAT_SNACK' })}>snacks - {snacks}</button>
-          <button onClick={() => this.handleSelection({ type: 'TAKE_NAP' })}>naps - {naps}</button>
-          <button onClick={() => this.handleSelection({ type: 'STUDY' })}>studies - {studies}</button>
-        </Controls>
-        <Face emoji={face} />
-      </>
-    );
-  }
-}
+export default Moods;
